cd7410d7ad0de06c09abd94055c5705f5eab22ce
KallkaGO <765830637@qq.com>
2023-02-18 20:26:28 +0800

    feat：完成注册接口的配置

--- src/request/module/login.ts
+++ src/request/module/login.ts
@@ -1,16 +1,26 @@
-import Request from '../index'
+import Request from "../index";
 
 const userLogin = (name: string, pwd: string) => {
-    return Request.post({
-        url: '/login',
-        data: {
-            username: name,
-            password:pwd
-        }
-    })
-    
-}
+  return Request.post({
+    url: "/login",
+    data: {
+      username: name,
+      password: pwd,
+    },
+  });
+};
+
+const userSign = (email: string, pwd: string) => {
+  return Request.post({
+    url: "/register/send",
+    data: {
+      email,
+      password: pwd,
+    },
+  });
+};
 
 export {
-    userLogin
-}
+    userLogin,
+    userSign
+};
